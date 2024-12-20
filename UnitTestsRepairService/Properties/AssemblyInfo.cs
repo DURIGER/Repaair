51ab06d101cf5fe3660348466c08adc2ef3742f4
DURIGER <117347941+DURIGER@users.noreply.github.com>
2024-12-20 13:17:43 +0300

    Add files via upload

---
+++ UnitTestsRepairService/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestsRepairService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestsRepairService")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c234ea3-2d5c-4a0d-ac77-ad2d13eeb051")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestsRepairService/TimeCalculationTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputerRepairRequests;
+using static ComputerRepairRequests.TimeCalculation;
+using Moq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System;
+    
+
+namespace UnitTestsRepairService
+{
+    [TestClass]
+    public class TimeCalculationTests
+    {
+        [TestMethod]
+        public void ReturnsCorrectAverage_WhenDataExists()
+        {
+            
+            var mockConnection = new Mock<IDbConnection>();
+            var mockCommand = new Mock<IDbCommand>();
+            var mockReader = new Mock<IDataReader>();
+
+            
+            mockConnection.Setup(conn => conn.CreateCommand()).Returns(mockCommand.Object);
+            mockConnection.Setup(conn => conn.Open()).Verifiable();
+
+            mockCommand.Setup(cmd => cmd.ExecuteReader()).Returns(mockReader.Object);
+            mockCommand.SetupGet(cmd => cmd.CommandText).Returns(It.IsAny<string>());
+
+            var data = new List<int> { 5, 7, 10 };
+            mockReader.SetupSequence(reader => reader.Read())
+                .Returns(true)
+                .Returns(true)
+                .Returns(true)
+                .Returns(false);
+
+            mockReader.SetupSequence(reader => reader.GetInt32(0)).Returns(() => data[0]).Returns(() => data[1]).Returns(() => data[2]);
+
+            var timeCalculation = new TimeCalculation(mockConnection.Object);
+
+            var result = timeCalculation.CalculateAverageCompletionTime();
+
+            Assert.AreEqual(7.33, result, 0.01); 
+            mockConnection.Verify(conn => conn.Open(), Times.Once);
+        }
+
+        [TestMethod]
+        public void ReturnsZero_WhenNoDataExists()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+            var mockCommand = new Mock<IDbCommand>();
+            var mockReader = new Mock<IDataReader>();
+
+            mockConnection.Setup(conn => conn.CreateCommand()).Returns(mockCommand.Object);
+            mockConnection.Setup(conn => conn.Open()).Verifiable();
+
+            mockCommand.Setup(cmd => cmd.ExecuteReader()).Returns(mockReader.Object);
+            mockCommand.SetupGet(cmd => cmd.CommandText).Returns(It.IsAny<string>());
+
+            mockReader.Setup(reader => reader.Read()).Returns(false);  
+
+            var timeCalculation = new TimeCalculation(mockConnection.Object);
+
+            var result = timeCalculation.CalculateAverageCompletionTime();
+
+            Assert.AreEqual(0, result);
+            mockConnection.Verify(conn => conn.Open(), Times.Once);
+        }
+
+        [TestMethod]
+        public void ReturnsSingleValue_WhenOneDataExists()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+            var mockCommand = new Mock<IDbCommand>();
+            var mockReader = new Mock<IDataReader>();
+
+            mockConnection.Setup(conn => conn.CreateCommand()).Returns(mockCommand.Object);
+            mockConnection.Setup(conn => conn.Open()).Verifiable();
+
+            mockCommand.Setup(cmd => cmd.ExecuteReader()).Returns(mockReader.Object);
+            mockCommand.SetupGet(cmd => cmd.CommandText).Returns(It.IsAny<string>());
+
+            mockReader.SetupSequence(reader => reader.Read())
+                .Returns(true)
+                .Returns(false); 
+
+            mockReader.Setup(reader => reader.GetInt32(0)).Returns(7);  
+
+            var timeCalculation = new TimeCalculation(mockConnection.Object);
+
+            var result = timeCalculation.CalculateAverageCompletionTime();
+
+            Assert.AreEqual(7, result);  
+            mockConnection.Verify(conn => conn.Open(), Times.Once);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ThrowsException_WhenDatabaseErrorOccurs()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+
+            mockConnection.Setup(conn => conn.Open()).Throws(new InvalidOperationException());
+
+            var timeCalculation = new TimeCalculation(mockConnection.Object);
+
+            timeCalculation.CalculateAverageCompletionTime(); 
+        }
+
+        [TestMethod]
+        public void ReturnsZero_WhenCompletionDateIsNull()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+            var mockCommand = new Mock<IDbCommand>();
+            var mockReader = new Mock<IDataReader>();
+
+            mockConnection.Setup(conn => conn.CreateCommand()).Returns(mockCommand.Object);
+            mockConnection.Setup(conn => conn.Open()).Verifiable();
+
+            mockCommand.Setup(cmd => cmd.ExecuteReader()).Returns(mockReader.Object);
+            mockCommand.SetupGet(cmd => cmd.CommandText).Returns(It.IsAny<string>());
+
+            mockReader.SetupSequence(reader => reader.Read())
+                .Returns(true)
+                .Returns(false); 
+
+            mockReader.Setup(reader => reader.GetInt32(0)).Returns(0);  
+
+            var timeCalculation = new TimeCalculation(mockConnection.Object);
+
+            var result = timeCalculation.CalculateAverageCompletionTime();
+
+            Assert.AreEqual(0, result);  
+            mockConnection.Verify(conn => conn.Open(), Times.Once);
+        }
+    }
+}
+   
+
